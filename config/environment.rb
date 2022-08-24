79e17870cedee4d9fc88e5d4c017c55aec015fd9
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-08-24 13:00:23 +0300

    ORM

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,25 @@ require_relative '../lib/student'
 DB = { conn: SQLite3::Database.new("db/school.db") }
 
 # RUN CODE FROM HERE
+
+#CREATE TABLE
+Student.create_table
+
+#CREATE A NEW STUDENT
+godfrey = Student.new(name: "Godfrey", age: 25)
+
+#ADD STUDENT TO DATABASE
+godfrey.add_to_db
+
+
+pp Student.show_all
+
+godfrey.name = "Onyonka"
+godfrey.age = 24
+godfrey.update_student 
+
+pp Student.show_all
+
+godfrey.delete_student 
+
+pp Student.show_all
